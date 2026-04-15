5b059dabd19ab3592304e4719a3686d70d638c2f
robergonzalezs-gif <rober.gonzalezs@duocuc.cl>
2026-04-14 23:58:08 -0400

    Update Persona.java
    Se añade un mensaje de salida y agradecimiento en la clase "Persona"

--- Persona.java
+++ Persona.java
@@ -2,5 +2,5 @@ public class Persona {
     String nombre;
     String apellido;
     int edad;
-    public void saludar() { System.out.println("Hola"); }
+    public void saludar() { System.out.println("Gracias por usar nuestro servicio de pruebas"); }
 }
